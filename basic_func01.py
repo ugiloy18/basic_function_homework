c898e941c0ca4e0fc6ba2f665f1bd9f2382a9d0c
ogiloy <ogiloykurbanyeva@gmail.com>
2025-06-27 09:08:35 +0500

    saqlandi

--- basic_func01.py
+++ basic_func01.py
@@ -1,4 +1,7 @@
 # Task: Create a function called return_zero that returns the integer 0
 # Function should:
 # 1. Take no parameters
-# 2. Return the integer value 0
+# 2. Return the integer value.
+def return_zero():
+    return 0
+print(return_zero())
