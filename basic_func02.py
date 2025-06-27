aaf65921f8413c30eeb325fd32364f1c52c89103
ogiloy <ogiloykurbanyeva@gmail.com>
2025-06-27 09:09:01 +0500

    saqlandi

--- basic_func02.py
+++ basic_func02.py
@@ -1,4 +1,7 @@
 # Task: Create a function called get_hello_world that returns the string "Hello World"
 # Function should:
 # 1. Take no parameters
-# 2. Return exactly "Hello World" as a string
+# 2. Return exactly "Hello World" as a string.
+def get_hello_world():
+    return "hello World"
+print(get_hello_world())
