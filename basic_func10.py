b2477b722db334b823cdf9e4dc61ee20a9b80d1a
ogiloy <ogiloykurbanyeva@gmail.com>
2025-06-27 09:14:23 +0500

    saqlandi

--- basic_func10.py
+++ basic_func10.py
@@ -3,3 +3,6 @@
 # 1. Take one parameter 'a'
 # 2. Return the negative value of 'a' (multiply by -1)
 # Example: if a=5, return -5; if a=-3, return 3
+def get_opposite_value():
+    return('3')
+print(get_opposite_value())
