86ccfaa41affb933772b81d3b59a2080141180d4
ogiloy <ogiloykurbanyeva@gmail.com>
2025-06-27 09:12:24 +0500

    saqlandi

--- basic_func07.py
+++ basic_func07.py
@@ -3,3 +3,6 @@
 # 1. Take one parameter 'a'
 # 2. Return the exact same value of 'a' without any modification
 # Example: if a=5, return 5
+def return_same_value():
+    return('5')
+print(return_same_value())
